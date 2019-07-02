27c03ea8cd2d37d9922696e11d20cb07e5ebd474
somefoo <50917034+somefoo@users.noreply.github.com>
2019-07-02 10:17:28 +0200

    Added game engine view for game dev.

---
+++ engine/game_controller.h
@@ -0,0 +1,26 @@
+#include "debug_draw_manager.h"
+#include "instance_manager.h"
+#include "key_event_manager.h"
+#include "sprite_manager.h"
+
+// Restricted view on the functions of the engine
+namespace game {
+//! @copydoc debug_draw_ge::draw_magenta(ivec2)
+void draw_magenta(ivec2 position) { debug_draw_ge::draw_magenta(position); }
+//! @copydoc debug_draw_ge::draw_green(ivec2)
+void draw_green(ivec2 position) { debug_draw_ge::draw_green(position); }
+
+//! @copydoc sprite_ge::load_sprite(std::string)
+sprite* load_sprite(std::string path) { return sprite_ge::load_sprite(path); }
+
+//! @copydoc instance_ge::instantiate()
+template <class T>
+T* instantiate() {
+  return instance_ge::instantiate<T>();
+}
+//! @copydoc sprite_ge::destroy(game_object)
+void destroy(game_object* o) { instance_ge::destroy(o); }
+
+//! @copydoc key_event_ge::is_pressed(unsigned char)
+bool is_pressed(unsigned char key) { return key_event_ge::is_pressed(key); }
+}  // namespace game
